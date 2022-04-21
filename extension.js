6cc273e2cb1d22b66c50b0abbfcdc4673919af44
fedricknishant <fednish@gmail.com>
2022-04-21 15:35:47 +0530

    added key bindings

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "turbo-print" is now active!');
+	console.log('Congratulations, your extension "turbo-python-print" is now active!');
 
 
 
@@ -21,7 +21,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('turbo-print.pythonPrint', async () => {
+	let disposable = vscode.commands.registerCommand('turbo-python-print.pythonPrint', async () => {
 		// The code you place here will be executed every time your command is executed
 
 
